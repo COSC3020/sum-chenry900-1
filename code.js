b27958baa2ee3d3bffefa3cce62b7fe207c4b3f8
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-01-23 11:16:31 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,5 @@
 function sum(a) {
-    if (a.length() == 1)
-        return a[0];
-    if (a.length() == 0)
+    if (a.length == 0)
         return 0;
     var total = a[0];
     for(var i = 1; i < a.length; i++) {
